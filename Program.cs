99425d81f96ccbdc890d6feab663cf3eca4c53f8
Jalon Duncan <duncanjalon@gmail.com>
2021-08-26 10:07:12 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+            Company HotStuff = new Company ();
+            // Create three employees
+            Employee Tommy = new Employee("Tommy");
+            Employee Brad = new Employee("Brad");
+            Employee Sara = new Employee("Sara");
+
+            // Assign the employees to the company
+
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
+        }
+    }
+}
